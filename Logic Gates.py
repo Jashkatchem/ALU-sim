8c8c6ad2a4d3465a090da3e43497078514d6066e
Joshua Swaby <jfishvoice@gmail.com>
2023-04-28 19:36:25 -0700

    Finalized our program.

--- Logic Gates.py
+++ Logic Gates.py
@@ -1,3 +1,4 @@
+# To begin we define the necessary logic gates for a full adder. These are the AND, OR, and XOR gates. 
 def AND_gate(a,b):
     if a == 1 and b == 1:
         return 1
@@ -23,20 +24,20 @@ def OR_gate(a,b):
         return 1
     else: 
         return 0
-    
+#Next we will implement the function that simulates a half adder by using our XOR and AND gates.  
 def Half_Adder(a,b):
     #print(a)
     #print(b)
     sum = XOR_gate(a,b)
     carry = AND_gate(a,b)
     return sum, carry
-
+#Then we will use two half adders and an OR gate to create a full adder. 
 def Full_Adder(a,b,c):
     sum1, carry1 = Half_Adder(a,b)
     sum2, carry2 = Half_Adder(sum1, c)
     carry3 = OR_gate(carry1, carry2)
     return sum2, carry3
-
+#Lastly I made a function that does the adding by using one half adder and 7 full adders to read the eight bits of our problem. 
 def eight_bit_add(a,b):
     # we need to take the lists of digits and plug them into a half adder followed by a series of full adders in order to produce an output string. 
     result = []
@@ -49,14 +50,14 @@ def eight_bit_add(a,b):
         sum, carry = Full_Adder(int(a[7-n]), int(b[7-n]), carry)
         result.insert(0, sum)
     # we need to check if there is still a carry and if so return an error. Otherwise, we need to concatenate result into a string and return it. 
-    #if carry == 1: 
-    #    return 00000000
+    if carry == 1: 
+        return "OVERFLOW ERROR"
     else:
         final = ''
         for val in result:
             final = final + str(val)
         return final
-# we need to call eight_bit_add on two values read from a text and then write the result back to the text. Then we need to refactor the whole thing. 
+# we need to call eight_bit_add on two values read from a text and then write the result back to the text. 
 file = open("Values.txt", "r")
 value1 = file.readline()
 value2 = file.readline()
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ALU-sim
 This is an abstracted simulation of an ALU. The goal is to read two binary values from a txt doc and then write their sum back into the text doc. The program will also use functional abstractions of the classic logic gates to work through binary addition. 
+To run the program you need to give it two eight bit binary values. They need to be typed into the first and second lines of Values.txt. 
+Only type the first twwo lines and only give it two 8 bit digits. Like so: 
+
+00000001
+00000010
+
+We are simply using python's built in lineread method. 
+Any other things entered into the Values.txt will produce innacurate or nonsensicle results. 
+The program will then read those values do its duty and write the results to the Results.txt. 
+If you gave it two valid eight bit values it will either give you the eight bit sum, or let you know the sum was 9 bits by writing OVERFLOW ERROR. 
